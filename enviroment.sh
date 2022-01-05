03cabbf165f99e2eb21ba5a576b86a66c3e96a08
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2022-01-04 22:31:10 -0300

    Arquivo de configuração de ambiente

---
+++ enviroment.sh
@@ -0,0 +1,41 @@
+# Atualizando os repositórios, como de praxe
+sudo apt update
+
+# Instalando dependências
+sudo apt install curl apt-transport-https
+
+# Instalando aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf aws/
+rm awscliv2.zip
+
+# Instalando github cli
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+
+# Instalando slack
+sudo snap install slack --classic
+
+## Configurando ambiente de desenvolvimento mobile
+
+# Instalando o android studio
+sudo snap install android-studio --classic
+
+# Inslando dart
+sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+sudo apt update
+sudo apt install dart
+
+echo 'export PATH="$PATH:/usr/lib/dart/bin"' >> ~/.profile
+
+# Instalando flutter
+sudo snap install flutter --classic
+flutter sdk-path
+
+## Fim mobile
+
