ad60ebce1034877e76e06ee8159282be23377422
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2024-10-08 19:44:07 -0300

    android: added android dev related configs

---
+++ android/.bashrc.d/android_settings.sh
@@ -0,0 +1,4 @@
+export ANDROID_HOME="$HOME/.local/android/sdk"
+export ANDROID_USER_HOME="$HOME/.local/android/data"
+export ANDROID_EMULATOR_HOME="$ANDROID_USER_HOME"
+export ANDROID_AVD_HOME="$ANDROID_USER_HOME"
