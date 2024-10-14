2d09c5c771ca7bd783cb75923be5bd1c2972e425
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2024-10-14 00:20:59 -0300

    npm: disabled corepack auto pin

--- npm/.bash_profile.d/npm_settings.sh
+++ npm/.bash_profile.d/npm_settings.sh
@@ -1,3 +1,4 @@
 export NPM_PACKAGES="${HOME}/.local/usr/lib/node_modules"
 export PATH="$PATH:$NPM_PACKAGES/bin"
 export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
+export COREPACK_ENABLE_AUTO_PIN=0
