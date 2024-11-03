7233abe25e325e8500e6b4c57f8085ae20342875
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2024-11-03 00:16:43 -0300

    heroku: added heruku cli autocomplete

---
+++ heroku/.bashrc.d/heroku.sh
@@ -0,0 +1,2 @@
+# heroku autocomplete setup
+HEROKU_AC_BASH_SETUP_PATH=/home/alcides/.cache/heroku/autocomplete/bash_setup && test -f $HEROKU_AC_BASH_SETUP_PATH && source $HEROKU_AC_BASH_SETUP_PATH;
