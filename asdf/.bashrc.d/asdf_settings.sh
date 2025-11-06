7af8a1e7770910d904b6bf46f07c2b01468108ca
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2025-11-05 21:33:58 -0300

    asdf: update asdf settings
    removed FLUTTER_ROOT variable assigment
    stoped sourcing asdf script as it is not a shell script anymore

--- asdf/.bashrc.d/asdf_settings.sh
+++ asdf/.bashrc.d/asdf_settings.sh
@@ -1,8 +1,3 @@
 export ASDF_CONFIG_FILE=$HOME/.config/asdf/asdfrc
 export ASDF_DIR=$HOME/.local/asdf/core
 export ASDF_DATA_DIR=$HOME/.local/asdf/data
-
-. "$ASDF_DIR/asdf.sh"
-. "$ASDF_DIR/completions/asdf.bash"
-
-export FLUTTER_ROOT="$(asdf where flutter)"
