50db87fa0b2e0a04105a9703765f8ff87d701686
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2025-11-06 19:04:25 -0300

    git: set new env vars for git ps1

--- git/.bashrc.d/git.sh
+++ git/.bashrc.d/git.sh
@@ -1,4 +1,6 @@
 export GIT_PS1_SHOWCOLORHINTS=1
 export GIT_PS1_SHOWDIRTYSTATE=1
+export GIT_PS1_SHOWUPSTREAM='auto'
+export GIT_PS1_SHOWUNTRACKEDFILES=1
 
 . /usr/share/git-core/contrib/completion/git-prompt.sh
