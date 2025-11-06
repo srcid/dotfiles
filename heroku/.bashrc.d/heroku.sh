bed84d528632b0258b9bc593a4fd25b330e51bb9
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2025-11-06 19:02:18 -0300

    heroku: removed heroku package

--- heroku/.bashrc.d/heroku.sh
+++
@@ -1,2 +0,0 @@
-# heroku autocomplete setup
-HEROKU_AC_BASH_SETUP_PATH=/home/alcides/.cache/heroku/autocomplete/bash_setup && test -f $HEROKU_AC_BASH_SETUP_PATH && source $HEROKU_AC_BASH_SETUP_PATH;
