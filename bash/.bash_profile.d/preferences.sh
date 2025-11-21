1b4f4e9fc040944e322ba6fb6f081bf994e896e1
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2025-11-21 20:11:30 -0300

    bash: added kwallet variables

--- bash/.bash_profile.d/preferences.sh
+++ bash/.bash_profile.d/preferences.sh
@@ -4,3 +4,8 @@ export HISTFILESIZE=-1
 export EDITOR="nvim"
 export SUDO_EDITOR=$EDITOR
 export VISUAL=$EDITOR
+
+# Needed to use KDE kwallet
+export PINENTRY_KDE_USE_WALLET=1
+export SSH_ASKPASS='/usr/bin/ksshaskpass'
+export SSH_ASKPASS_REQUIRE='prefer'
