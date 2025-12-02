a75c843218bde09f0e1cb25a6fe89fd19ad831ed
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2025-12-02 20:00:39 -0300

    npm: set NPM_CONFIG_USERCONFIG env var, and moved .vscode to user config folder

--- npm/.bash_profile.d/npm.sh
+++ npm/.bash_profile.d/npm.sh
@@ -2,3 +2,4 @@ export NPM_PACKAGES="${HOME}/.local/usr/lib/node_modules"
 export PATH="$PATH:$NPM_PACKAGES/bin"
 export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
 export COREPACK_ENABLE_AUTO_PIN=0
+export NPM_CONFIG_USERCONFIG="$HOME/.config/npm/npmrc"
