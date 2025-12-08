ef609196a40e2b5cc745c65d73aba9d6f19e465f
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2025-12-08 15:45:00 -0300

    npm: changed prefix to .local

--- npm/.bash_profile.d/npm.sh
+++ npm/.bash_profile.d/npm.sh
@@ -1,5 +1,2 @@
-export NPM_PACKAGES="${HOME}/.local/lib/node_modules"
-export PATH="$PATH:$NPM_PACKAGES/bin"
-export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
 export COREPACK_ENABLE_AUTO_PIN=0
 export NPM_CONFIG_USERCONFIG="$HOME/.config/npm/npmrc"
