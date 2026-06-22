347cfa94b589143bb73576e943b656789e91927b
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2026-06-22 16:13:24 -0300

    asdf: moved data dir and checking if asdf is installed before attempring to export env vars

--- asdf/.bash_profile.d/asdf.sh
+++ asdf/.bash_profile.d/asdf.sh
@@ -1,3 +1,7 @@
 export ASDF_CONFIG_FILE=$HOME/.config/asdf/asdfrc
-export ASDF_DATA_DIR=$HOME/.asdf
+export ASDF_DATA_DIR=$HOME/.local/opt/asdf
 export PATH=$ASDF_DATA_DIR/shims:$PATH
+
+type asdf &> /dev/null || return 0
+
+export FLUTTER_ROOT="$(asdf where flutter)"
