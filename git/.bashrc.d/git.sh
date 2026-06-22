30885dc3e53b4076024ab13e9a4fb1fa3923462a
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2026-06-22 16:47:09 -0300

    git: checking git prompt script path before load it

--- git/.bashrc.d/git.sh
+++ git/.bashrc.d/git.sh
@@ -1,1 +1,4 @@
-source /usr/share/git-core/contrib/completion/git-prompt.sh
+# Need to manually loads git shell script on Fedora
+if [ -f /usr/share/git-core/contrib/completion/git-prompt.sh ]; then
+    source /usr/share/git-core/contrib/completion/git-prompt.sh
+fi
