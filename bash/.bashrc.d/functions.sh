3a27728d1fb4b37796f0e98bc991dca24589a7f1
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2026-07-03 22:22:46 -0300

    bash(functions): added helper functions to work with clipboard

--- bash/.bashrc.d/functions.sh
+++ bash/.bashrc.d/functions.sh
@@ -11,3 +11,37 @@ randint() {
   end=$2
   echo $(( RANDOM % (end - start + 1) + start ))
 }
+
+cbcopy() {
+    local X11_CPCMD='xclip -selection clipboard -i'
+    local WL_CPCMD='wl-copy'
+
+    case $XDG_SESSION_TYPE in
+        wayland)
+            $WL_CPCMD
+            ;;
+        X11)
+            $X11_CPCMD
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+cbpaste() {
+    local X11_PASTECMD='xclip -selection clipboard -o'
+    local WL_PASTECMD='wl-paste -n'
+
+    case $XDG_SESSION_TYPE in
+        wayland)
+            $WL_PASTECMD
+            ;;
+        X11)
+            $X11_PASTECMD
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
